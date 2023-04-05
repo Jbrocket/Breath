1769ac87a8f601a013d6413ab1dd24629674f646
Jonathan Brockett <Jbrockett747@gmail.com>
2023-04-05 18:10:30 -0400

    added basic client functionality to connect

--- client.py
+++ client.py
@@ -1,72 +1,134 @@
 import pygame 
 import sys 
+import socket
+import json
+
+global width
+global height
+global screen
   
-  
-# initializing the constructor 
-pygame.init() 
-  
-# screen resolution 
-res = (720,720) 
-  
-# opens up a window 
-screen = pygame.display.set_mode(res) 
-  
-# white color 
-color = (255,255,255) 
-  
-# light shade of the button 
-color_light = (170,170,170) 
-  
-# dark shade of the button 
-color_dark = (100,100,100) 
-  
-# stores the width of the 
-# screen into a variable 
-width = screen.get_width() 
-  
-# stores the height of the 
-# screen into a variable 
-height = screen.get_height() 
-  
-# defining a font 
-smallfont = pygame.font.SysFont('Corbel',35) 
-  
-# rendering a text written in 
-# this font 
-text = smallfont.render('quit' , True , color) 
-  
-while True: 
-      
-    for ev in pygame.event.get(): 
-          
-        if ev.type == pygame.QUIT: 
-            pygame.quit() 
-              
-        #checks if a mouse is clicked 
-        if ev.type == pygame.MOUSEBUTTONDOWN: 
-              
-            #if the mouse is clicked on the 
-            # button the game is terminated 
-            if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40: 
+class ClientSocket:
+    
+    def __init__(self, host=None, port=None):
+        self.host = host
+        self.port = port
+        self.send_socket = None
+        self.recv_socket = None
+        
+    def connect(self, host=None, port=None, name="jbrock"):
+        self.send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        if host:
+            self.host = host
+        if port:
+            self.port = port
+            
+        self.recv_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.recv_socket.bind(("127.0.0.1", 0))
+        
+        return self.connect_ack(name)
+        
+    def connect_ack(self, name):
+        msg = json.dumps({"player": name, "recv_port": self.recv_socket.getsockname()[1], "address": socket.gethostname()})
+        print(msg)
+        self.send_socket.sendto(msg.encode(), (self.host, self.port))
+        
+        data, addr = self.recv_socket.recvfrom(65536)
+        
+        return data
+    
+    def recv_mesg(self):
+        data, addr = self.recv_socket.recvfrom(65536)
+        
+        
+def get_host_and_client():
+    color = (255,255,255) 
+    color_light = (170,170,170) 
+    color_dark = (100,100,100) 
+    
+    smallfont = pygame.font.SysFont('Corbel',35)
+    text_connected = smallfont.render('connected' , True , color) 
+    text_not_connected = smallfont.render('not connected' , True , color) 
+    client = ClientSocket()
+    server = ""
+    port = ""
+    connected = False
+    option = 1
+    
+    while True: 
+        
+        for ev in pygame.event.get(): 
+            
+            if ev.type == pygame.QUIT: 
                 pygame.quit() 
-                  
-    # fills the screen with a color 
-    screen.fill((60,25,60)) 
-      
-    # stores the (x,y) coordinates into 
-    # the variable as a tuple 
-    mouse = pygame.mouse.get_pos() 
-      
-    # if mouse is hovered on a button it 
-    # changes to lighter shade 
-    if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40: 
-        pygame.draw.rect(screen,color_light,[width/2,height/2,140,40]) 
-          
-    else: 
-        pygame.draw.rect(screen,color_dark,[width/2,height/2,140,40]) 
-      
-    # superimposing the text onto our button 
-    screen.blit(text , (width/2+50,height/2)) 
-      
-    # updates the frames of the game 
-    pygame.display.update() 
+                
+            if ev.type == pygame.MOUSEBUTTONDOWN:
+                if not connected:
+                    if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40: 
+                        client.connect(server, port, "jbrock")
+                        connected = True
+                        # pygame.quit()
+            if ev.type == pygame.KEYDOWN:
+                if ev.key == pygame.K_BACKSPACE:
+                    if option == 1:
+                        server = server[:-1]
+                    else:
+                        port = port[:-1]
+                elif ev.unicode == "1":
+                    option = 1
+                elif ev.unicode == "2":
+                    option = 2
+                else:
+                    if option == 1:
+                        server += ev.unicode
+                    else:
+                        port += ev.unicode
+                    
+                    
+        # fills the screen with a color 
+        screen.fill((60,25,60)) 
+        
+        # stores the (x,y) coordinates into 
+        # the variable as a tuple 
+        mouse = pygame.mouse.get_pos() 
+        
+        # if mouse is hovered on a button it 
+        # changes to lighter shade 
+        if not connected:
+            if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40: 
+                pygame.draw.rect(screen,color_light,[width/2-50,height/2,140,40]) 
+                
+            else: 
+                pygame.draw.rect(screen,color_dark,[width/2-50,height/2,140,40]) 
+        
+        # superimposing the text onto our button 
+        if not connected:
+            screen.blit(text_not_connected , (width/2-50,height/2)) 
+        else:
+            screen.blit(text_connected , (width/2-50,height/2)) 
+        
+        screen.blit(smallfont.render(server, True, color), (width/2-50, height/4))
+        screen.blit(smallfont.render(port, True, color), (width/2-50, height/3))
+        screen.blit(smallfont.render("1 server", True, color), (width/2-200, height/4))
+        screen.blit(smallfont.render("2 port", True, color), (width/2-200, height/3))
+        
+        pygame.display.update()
+    
+def in_game_loop():
+    pass
+
+def main():
+    pygame.init()
+    res = (600,500) 
+
+    global width
+    global height
+    global screen
+    
+    screen = pygame.display.set_mode(res) 
+    width = screen.get_width() 
+    height = screen.get_height() 
+    
+    get_host_and_client()
+    
+if __name__ == "__main__":
+    main()
---
+++ megalib.py
@@ -0,0 +1,63 @@
+import random
+
+class Player:
+    def __init__(self, name: str, x = None, y = None):
+        self.name = name
+        self.O2_level = 100
+        self.x = x
+        self.y = y
+        
+    def O2_depletion(self, time):
+        self.O2_level -= 1 * time//10
+        
+        if self.O2_level <= 0:
+            return False
+        
+        return True
+        
+    def collect_O2(self):
+        self.O2_level += 20 if self.O2_level + 20 <= 100 else 100
+        
+    def move_up(self):
+        self.y += 1
+        
+    def move_down(self):
+        self.y -= 1
+        
+    def move_right(self):
+        self.x += 1
+        
+    def move_left(self):
+        self.y -= 1
+
+class O2Tanks:
+    def __init__(self):
+        self.O2_tanks = list()
+        self.O2_generator = O2TankGenerator()
+        
+    def create_tanks(self, num):
+        self.empty_list()
+        
+        for _ in range(num):
+            self.O2_tanks.append(self.O2_generator.generate())
+        
+    def empty_list(self):
+        self.O2_tanks = list()
+
+class O2Tank:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class O2TankGenerator:
+    def __init__(self):
+        pass
+        
+    def generate(self):
+        x = random.randint(0, 64)
+        y = random.randint(0, 64)
+        
+        return O2Tank(x, y)
+
+class FireExtinguisher:
+    pass
