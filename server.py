03c2f9041f5480521adedc79328861f0f9823027
ppenderg <ppenderg@nd.edu>
2023-04-05 19:25:29 -0400

    early player update ?

--- server.py
+++ server.py
@@ -1,14 +1,10 @@
-import socket, json, time, threading; 
-
-
-
-
+import socket, json, time, threading;
 
 class GameServer:
 
     def __init__(self, port_no):
         self.port_no = port_no;
-        self.user_list = [];
+        self.user_list = {};
     
 
 
@@ -34,7 +30,7 @@ class GameServer:
                 #if(payload["args"]["username"] not in self.user_listAAAAA):
                 if(True):
                     # Do some authentication or something here! 
-                    self.user_list.append({"username": payload["args"]["username"], "status": "loading", "address": addr, "last_heard_from": time.time()});
+                    self.user_list[payload["args"]["username"]] = {"status": "loading", "address": addr, "last_heard_from": time.time(), "position": {"x": 0, "y": 0}};
 
                     response = {"method": "accept_connection", "args": None};
                 else:
@@ -49,7 +45,12 @@ class GameServer:
                 pass;
 
             elif(payload["method"] == "send_player_update"):
-                pass;
+                print(payload["args"]);
+                self.update_player_position(payload["args"]);
+
+                response = self.user_list[payload["args"]["username"]]["position"];
+
+                self.sock.sendto(json.dumps(response).encode(), addr);
 
             else:
                 print("error", payload);
@@ -60,6 +61,22 @@ class GameServer:
 
 
 
+
+    def update_player_position(self, args):
+        try:
+            username = args["username"];
+            input_val = args["input"];
+            if(input_val == "up"):
+                self.user_list[username]["position"]["y"] += 1;
+            elif(input_val == "down"):
+                self.user_list[username]["position"]["y"] -= 1;
+            elif(input_val == "left"):
+                self.user_list[username]["position"]["x"] -= 1;
+            elif(input_val == "right"):
+                self.user_list[username]["position"]["x"] += 1;
+            print(self.user_list[username]["position"]);
+        except:
+            pass;
 
 
 
--- testclient.py
+++ testclient.py
@@ -8,3 +8,17 @@ payload = {"method": sys.argv[1], "args": {"username": sys.argv[2]}};
 msg = f"{len(json.dumps(payload)):>16}{json.dumps(payload)}";
 sock.sendto(msg.encode(), ('localhost', 6969));
 print(sock.recvfrom(64000)[0].decode());
+
+payload = {"method": "send_player_update", "args" : {"username": sys.argv[2], "input": "up"}};
+msg = f"{len(json.dumps(payload)):>16}{json.dumps(payload)}";
+sock.sendto(msg.encode(), ('localhost', 6969));
+print(sock.recvfrom(64000)[0].decode());
+
+payload = {"method": "send_player_update", "args" : {"username": sys.argv[2], "input": "left"}};
+msg = f"{len(json.dumps(payload)):>16}{json.dumps(payload)}";
+sock.sendto(msg.encode(), ('localhost', 6969));
+print(sock.recvfrom(64000)[0].decode());
+payload = {"method": "send_player_update", "args" : {"username": sys.argv[2], "input": "down"}};
+msg = f"{len(json.dumps(payload)):>16}{json.dumps(payload)}";
+sock.sendto(msg.encode(), ('localhost', 6969));
+print(sock.recvfrom(64000)[0].decode());
